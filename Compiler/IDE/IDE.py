983a95fe095630ab99233d6a5453c332a39cd6fe
GChacon8 <gabriel.chacon.alfaro@gmail.com>
2023-06-04 17:41:02 -0600

    Cambios funcionales en el IDE

--- Compiler/IDE/IDE.py
+++ Compiler/IDE/IDE.py
@@ -1,9 +1,9 @@
+import sys
+sys.path.append("..")
 from Compiler import *
 import tkinter as tk
 from tkinter import messagebox
 from tkinter.filedialog import asksaveasfilename, askopenfilename
-import sys
-sys.path.append("..")
 
 
 class IDE(object):
@@ -14,6 +14,7 @@ class IDE(object):
         self.master.resizable(False, False)
         self.master.configure(bg='#1E1E1E')
         self.saved = False
+        self.filepath = ""
         self.create_widgets()
 
     def create_widgets(self):
@@ -24,9 +25,10 @@ class IDE(object):
         prog_menu = tk.Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Program", menu=prog_menu)
 
-        filemenu.add_command(label="New File", command=self.new_file)
+        filemenu.add_command(label="New", command=self.new_file)
         filemenu.add_command(label="Open File", command=self.open_file)
-        filemenu.add_command(label="Save File", command=self.save_file)
+        filemenu.add_command(label="Save As", command=self.save_file_as)
+        filemenu.add_command(label="Save", command=self.save_file)
         
         prog_menu.add_command(label="Compile", command=self.compile_program)
         prog_menu.add_command(label="Run", command=self.run_program)
@@ -38,6 +40,7 @@ class IDE(object):
         self.coding_area = tk.Text(root, height=21, width=129, bg= "#1E1E1E", fg='aqua')
         self.coding_area.place(x=50,y=10)
         self.coding_area.bind('<Key>', self.line_number)
+        self.coding_area.bind('<Key>', self.changes_made)
         self.coding_area.bind('<Motion>', self.line_number)
         self.coding_area.bind('<MouseWheel>', self.line_number)
         self.coding_area.bind('<Tab>', self.insert_spaces)
@@ -48,22 +51,19 @@ class IDE(object):
         self.error_area = tk.Text(root, height=10, width=134, bg= "#1E1E1E", fg='light gray', state='disabled')
         self.error_area.place(x=10,y=470)
         
-        self.console_area = tk.Text(self.master)
-        self.console_area = tk.Text(root, height=10, width=134, bg= "#1E1E1E", fg='light gray', state='disabled')
-        self.console_area.place(x=10,y=470)
+        self.output_area = tk.Text(self.master)
+        self.output_area = tk.Text(root, height=10, width=134, bg= "#1E1E1E", fg='light gray', state='disabled')
+        self.output_area.place(x=10,y=470)
         
         self.lineno_area = tk.Text(self.master)
         self.lineno_area = tk.Text(root, height=25, width=3, bg= '#1E1E1E', fg='light gray', bd=0)
         self.lineno_area.place(x=10,y=10)
         self.lineno_area.config(state='disabled')
 
-        self.console_button = tk.Button(self.master)
-        self.console_button = tk.Button(root, text="Console", command= self.display_console, bg='#1E1E1E', fg='light gray', font= ('Segoe UI', '10', 'bold', 'underline'), bd=0)
-        self.console_button.place(x = 15, y = 440)
+        self.output = tk.Label(self.master)
+        self.output = tk.Label(root, text= "Output", font= ('Segoe UI', '10', 'bold'), bg= '#1E1E1E', fg='light gray')
+        self.output.place(x=10,y=440)
 
-        self.errors_button = tk.Button(self.master)
-        self.errors_button = tk.Button(root, text="Errors", command= self.display_errors, bg='#1E1E1E', fg='light gray', font= ('Segoe UI', '10'), bd=0)
-        self.errors_button.place(x = 80, y = 440)
 
         # Habilitar la funcionalidad de deshacer y rehacer
         self.coding_area.configure(undo=True)
@@ -76,22 +76,32 @@ class IDE(object):
         self.coding_area.bind('<Control-y>', self.redo)
 
     def run_program(self):
-        result = run_code(self.coding_area.get("1.0", "end"))
-        if isinstance(result, list):
-            self.console_area.configure(state='normal')
-            self.console_area.delete("1.0", tk.END)
-            for ele in result:
-                self.console_area.insert(tk.END, ele + "\n")
-            self.console_area.configure(state='disabled')
+        if self.filepath != "":
+            with open(self.filepath, "r") as file:
+                result = run_code(file.read())
+            if isinstance(result, list):
+                self.output_area.configure(state='normal')
+                self.output_area.delete("1.0", tk.END)
+                for ele in result:
+                    self.output_area.insert(tk.END, ele + "\n")
+                self.output_area.configure(state='disabled')
+        else:
+            tk.messagebox.showwarning("Warning", "Debe guardar el archivo antes de ejecutar el programa")
+            self.save_file_as()
 
     def compile_program(self):
-        result = compile_code(self.coding_area.get("1.0", "end"))
-        if isinstance(result, list):
-            self.console_area.configure(state='normal')
-            self.console_area.delete("1.0", tk.END)
-            for ele in result:
-                self.console_area.insert(tk.END, ele + "\n")
-            self.console_area.configure(state='disabled')
+        if self.filepath != "":
+            with open(self.filepath, "r") as file:
+                result = run_code(file.read())
+            if isinstance(result, list):
+                self.output_area.configure(state='normal')
+                self.output_area.delete("1.0", tk.END)
+                for ele in result:
+                    self.output_area.insert(tk.END, ele + "\n")
+                self.output_area.configure(state='disabled')
+        else:
+            tk.messagebox.showwarning("Warning", "Debe guardar el archivo antes de compilar el programa")
+            self.save_file_as()
 
     def redo(self, event=None):
         # Rehacer la última acción deshecha
@@ -104,38 +114,39 @@ class IDE(object):
         # Elimina el contenido del área de texto y muestra un mensaje.
         self.coding_area.delete("1.0", tk.END)
         messagebox.showinfo("Nuevo archivo", "Se ha creado un nuevo archivo.")
+        self.filepath = ""
         self.saved = False
 
     def open_file(self):
         # Abre un cuadro de diálogo para seleccionar un archivo y muestra su contenido en el área de texto.
-        filepath = askopenfilename(defaultextension=".br", filetypes=[("Text Files", "*.br"), ("All Files", "*.*")])
-        if filepath:
-            with open(filepath, "r") as file:
+        self.filepath = askopenfilename(defaultextension=".br", filetypes=[("Text Files", "*.br"), ("All Files", "*.*")])
+        if self.filepath:
+            with open(self.filepath, "r") as file:
                 self.coding_area.delete("1.0", tk.END)
                 self.coding_area.insert(tk.END, file.read())
+        self.saved = True
 
-    def save_file(self):
+    def save_file_as(self):
         # Abre un cuadro de diálogo para seleccionar la ubicación y el nombre del archivo y guarda el contenido del área de texto.
-        filepath = asksaveasfilename(defaultextension=".br", filetypes=[("Text Files", "*.br"), ("All Files", "*.*")])
-        print(filepath)
-        if filepath:
-            with open(filepath, "w") as file:
+        self.filepath = asksaveasfilename(defaultextension=".br", filetypes=[("Text Files", "*.br"), ("All Files", "*.*")])
+        print(self.filepath)
+        if self.filepath:
+            with open(self.filepath, "w") as file:
                 file.write(self.coding_area.get("1.0", tk.END))
             messagebox.showinfo("Guardar archivo", "El archivo se ha guardado correctamente.")
 
         self.saved = True
 
-    def display_console(self):
-        self.error_area.place_forget()
-        self.console_area.place(x=10,y=470)
-        self.console_button.config(font= ('Segoe UI', '10', 'bold', 'underline'))
-        self.errors_button.config(font= ('Segoe UI', '10'))
-    
-    def display_errors(self):
-        self.console_area.place_forget()
-        self.error_area.place(x=10,y=470)
-        self.errors_button.config(font= ('Segoe UI', '10', 'bold', 'underline'))
-        self.console_button.config(font= ('Segoe UI', '10'))
+    def save_file(self):
+        print(self.filepath)
+        if self.filepath:
+            with open(self.filepath, "w") as file:
+                file.write(self.coding_area.get("1.0", tk.END))
+            messagebox.showinfo("Guardar archivo", "El archivo se ha guardado correctamente.")
+        else:
+            self.save_file_as()
+
+        self.saved = True
 
     def line_number(self, event):
         last_line = self.coding_area.index(tk.END)
@@ -148,6 +159,9 @@ class IDE(object):
             self.lineno_area.insert(tk.END, i+1)
             self.lineno_area.insert(tk.END, "\n")
         self.lineno_area.config(state="disabled")
+
+    def changes_made(self, event):
+        self.saved = False
 
     def insert_spaces(self, event):
         self.coding_area.insert(tk.INSERT, " " * 4)
